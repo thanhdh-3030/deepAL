9f50f656a20ea3ff1943493a7264303af8fa7fc5
Thanh Dang <dang.hong.thanh@sun-asterisk.com>
2023-05-09 00:06:53 +0700

    add wandb and edit training pipline

--- main.sh
+++ main.sh
@@ -1,8 +1,10 @@
 python main.py \
-    -a LossPredictionLoss \
+    -a EntropySamplingDropout \
     -s 5000 \
     -q 15000 \
     -b 2500 \
     -d CIFAR10 \
     -t 6 \
-    -g 0
+    -g 0 \
+    -c \
+    -w \
